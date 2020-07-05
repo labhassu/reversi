22e0a1fcb25e62c774ff9b31e9ce409e4d87e97f
minmin0530 <izumiyoshiki@icloud.com>
2020-07-05 22:25:37 +0900

    layout_correction_button

--- reversi/reversi/GameViewController.swift
+++ reversi/reversi/GameViewController.swift
@@ -11,11 +11,35 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
+    
+    @IBOutlet weak var board: UIStackView!
     let game = GameClass()
     override func viewDidLoad() {
         super.viewDidLoad()
         print("viewDidLoad")
+        
+        var tag : Int = 0
+        for views in board.subviews {
+            for view in views.subviews {
+                let v = view as! UIButton
+                v.tag = tag
+                if tag == 35 || tag == 28 {
+                    v.setImage(UIImage(named: "black") , for: UIControl.State.normal)
+                    v.tintColor = UIColor.black
+                } else if tag == 36 || tag == 27 {
+                    v.setImage(UIImage(named: "white") , for: UIControl.State.normal)
+                    v.tintColor = UIColor.white
+                } else {
+                    v.setImage(UIImage(named: "green") , for: UIControl.State.normal)
+                    v.tintColor = UIColor(displayP3Red: 0.0, green: 0.5, blue: 0.0, alpha: 1.0)
+                }
+                v.setTitle("", for: UIControl.State.normal)
+                tag += 1
+            }
+        }
+        
         game.test()
+        /*
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
@@ -31,6 +55,7 @@ class GameViewController: UIViewController {
             view.showsFPS = true
             view.showsNodeCount = true
         }
+ */
     }
 
     override var shouldAutorotate: Bool {
